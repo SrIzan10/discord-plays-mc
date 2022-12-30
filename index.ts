4d251caf26655e891617c3da62271c33ff5e584d
Izan Gil <izan@srizan.ml>
2022-12-30 17:28:06 +0100

    feat: 1.0.0 i think

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as colorette from "colorette";
 import mineflayer from "mineflayer";
 import { mineflayer as mineflayerViewer } from "prismarine-viewer";
-import { Client, GatewayIntentBits } from "discord.js";
+import { ActivityType, Client, GatewayIntentBits } from "discord.js";
 import inventoryViewer from "mineflayer-web-inventory";
 import "dotenv/config";
 
@@ -22,118 +22,109 @@ const bot = mineflayer.createBot({
 let stopResponses: boolean = false
 
 client.on("messageCreate", async (message) => {
+	if (message.author.id === client.user!.id) return
 	if (
 		message.channel.id !== "1057648908630757426" &&
-		message.channel.id !== "1057744953545072710" &&
-		stopResponses === true
-	)
-		return;
-	const items = bot.inventory.items().map((item) => {
-		return item.displayName;
-	});
-	let entity;
-	switch (message.content) {
-		case "hold w":
-			bot.setControlState("forward", true);
-			break;
-		case "hold s":
-			bot.setControlState("back", true);
-			break;
-		case "hold a":
-			bot.setControlState("left", true);
-			break;
-		case "hold d":
-			bot.setControlState("right", true);
-			break;
-		case "w":
-			bot.setControlState("forward", true);
-			setTimeout(function () {
-				bot.setControlState("forward", false);
-			}, 100);
-			break;
-		case "s":
-			bot.setControlState("back", true);
-			setTimeout(function () {
-				bot.setControlState("back", false);
-			}, 100);
-			break;
-		case "a":
-			bot.setControlState("left", true);
-			setTimeout(function () {
-				bot.setControlState("left", false);
-			}, 100);
-			break;
-		case "d":
-			bot.setControlState("right", true);
-			setTimeout(function () {
-				bot.setControlState("right", false);
-			}, 100);
-			break;
-		case "sprint":
-			bot.setControlState("sprint", true);
-			break;
-		case "stop":
-			bot.clearControlStates();
-			break;
-		case "jump":
-			bot.setControlState("jump", true);
-			bot.setControlState("jump", false);
-			break;
-		case "hold jump":
-			bot.setControlState("jump", true);
-			break;
-		case "attack":
-			entity = bot.nearestEntity();
-			if (entity) {
-				bot.attack(entity);
-			} else {
-				bot.chat("no nearby entities");
-			}
-			break;
-		case "mount":
-			entity = bot.nearestEntity((entity) => {
-				return entity.name === "minecart" || entity.name === "boat";
-			});
-			if (entity) {
-				bot.mount(entity);
-			} else {
-				bot.chat("No hay objetos cerca!");
-			}
-			break;
-		case "dismount":
-			bot.dismount();
-			break;
-		case "vehicle forward":
-			bot.moveVehicle(0.0, 1.0);
-			break;
-		case "vehicle backward":
-			bot.moveVehicle(0.0, -1.0);
-			break;
-		case "vehicle left":
-			bot.moveVehicle(1.0, 0.0);
-			break;
-		case "vehicle right":
-			bot.moveVehicle(-1.0, 0.0);
-			break;
-		case "pos":
-			bot.chat(bot.entity.position.toString());
-			break;
-		case "items":
-			bot.chat(`${items.join(", ")}`);
-			break;
-		case "look left":
-			bot.look(bot.entity.yaw + 45, bot.entity.pitch);
-			break;
-		case "look right":
-			bot.look(bot.entity.yaw - 45, bot.entity.pitch);
-			break;
-		case "look up":
-			bot.look(bot.entity.yaw, bot.entity.pitch + 45);
-			break;
-		case "look down":
-			bot.look(bot.entity.yaw, bot.entity.pitch - 45);
-			break;
+		message.channel.id !== "1057744953545072710"
+	) return;
+
+	if (stopResponses !== true) {
+		const items = bot.inventory.items().map((item) => {
+			return item.displayName;
+		});
+		let entity;
+		switch (message.content) {
+			case "hold w":
+				bot.setControlState("forward", true);
+				break;
+			case "hold s":
+				bot.setControlState("back", true);
+				break;
+			case "hold a":
+				bot.setControlState("left", true);
+				break;
+			case "hold d":
+				bot.setControlState("right", true);
+				break;
+			case "w":
+				bot.setControlState("forward", true);
+				setTimeout(function () {
+					bot.setControlState("forward", false);
+				}, 100);
+				break;
+			case "s":
+				bot.setControlState("back", true);
+				setTimeout(function () {
+					bot.setControlState("back", false);
+				}, 100);
+				break;
+			case "a":
+				bot.setControlState("left", true);
+				setTimeout(function () {
+					bot.setControlState("left", false);
+				}, 100);
+				break;
+			case "d":
+				bot.setControlState("right", true);
+				setTimeout(function () {
+					bot.setControlState("right", false);
+				}, 100);
+				break;
+			case "sprint":
+				bot.setControlState("sprint", true);
+				break;
+			case "stop":
+				bot.clearControlStates();
+				break;
+			case "jump":
+				bot.setControlState("jump", true);
+				bot.setControlState("jump", false);
+				break;
+			case "hold jump":
+				bot.setControlState("jump", true);
+				break;
+			case "attack":
+				entity = bot.nearestEntity();
+				if (entity) {
+					bot.attack(entity);
+				} else {
+					bot.chat("no nearby entities");
+				}
+				break;
+			case "mount":
+				entity = bot.nearestEntity((entity) => {
+					return entity.name === "minecart" || entity.name === "boat";
+				});
+				if (entity) {
+					bot.mount(entity);
+				} else {
+					bot.chat("No hay objetos cerca!");
+				}
+				break;
+			case "dismount":
+				bot.dismount();
+				break;
+			case "vehicle forward":
+				bot.moveVehicle(0.0, 1.0);
+				break;
+			case "vehicle backward":
+				bot.moveVehicle(0.0, -1.0);
+				break;
+			case "vehicle left":
+				bot.moveVehicle(1.0, 0.0);
+				break;
+			case "vehicle right":
+				bot.moveVehicle(-1.0, 0.0);
+				break;
+			case "pos":
+				bot.chat(bot.entity.position.toString());
+				break;
+			case "items":
+				bot.chat(`${items.join(", ")}`);
+				break;
+		}
 	}
+
 	if (
 		message.content.includes("execute") &&
 		message.author.id === "703974042700611634"
@@ -158,9 +149,10 @@ client.on("messageCreate", async (message) => {
 		`${colorette.bgYellow("MSG")} => message ${message.content} recieved`
 	);
 });
-client.on("ready", () =>
-	console.log(colorette.bgGreen("Logged onto Discord!"))
-);
+client.on("ready", () => {
+	console.log(colorette.bgGreen("Logged onto Discord!"));
+	client.user?.setActivity({name: 'tus mensajes', type: ActivityType.Watching})
+});
 
 bot.on("login", () => {
 	console.log(colorette.bgGreen("Logged onto Minecraft!"));
